40b3d065abcfe46ac0be874f475e037e5ee4f4c1
Sandor Jonas <sandorjanosjonas@gmail.com>
2022-12-12 15:24:16 +1000

    Completed part 2 of Day 3: Rucksack Reorganization.

--- src/main/java/adventofcode/year2022/day03/Part2.java
+++ src/main/java/adventofcode/year2022/day03/Part2.java
@@ -8,7 +8,64 @@ import java.util.List;
 public class Part2 {
 
     public static void main(String[] args) {
+        List<String> contents = readFile("src/main/java/adventofcode/year2022/day03/input.txt");
+        int prioritySum = 0;
+        for (int priority : getPriorityValues(getMatches(getGroups(contents)))) {
+            prioritySum += priority;
+        }
 
+        System.out.println(prioritySum);
+    }
+
+    public static List<List<String>> getGroups(List<String> rucksacks) {
+        List<String> group = new ArrayList<>();
+        List<List<String>> groups = new ArrayList<>();
+
+        for (String rucksack : rucksacks) {
+            if (group.size() == 3) {
+                groups.add(group);
+                group = new ArrayList<>();
+            }
+
+            group.add(rucksack);
+        }
+        groups.add(group);
+
+        return groups;
+    }
+
+    public static List<String> getMatches(List<List<String>> groups) {
+        List<String> matches = new ArrayList<>();
+
+        for (List<String> group : groups) {
+            String secondElfsRucksack = group.get(1);
+            String thirdElfsRucksack = group.get(2);
+
+            for (String firstElfsRucksacksParts : group.get(0).split("")) {
+                if (secondElfsRucksack.contains(firstElfsRucksacksParts) && thirdElfsRucksack.contains(firstElfsRucksacksParts)) {
+                    matches.add(firstElfsRucksacksParts);
+                    break;
+                }
+            }
+        }
+
+        return matches;
+    }
+
+    public static List<Integer> getPriorityValues(List<String> matches) {
+        List<Integer> priorityValues = new ArrayList<>();
+
+        for (String match : matches) {
+            int asciiValue = match.charAt(0);
+
+            if (asciiValue <= 90) {
+                priorityValues.add(asciiValue - 38);
+            } else {
+                priorityValues.add(asciiValue - 96);
+            }
+        }
+
+        return priorityValues;
     }
 
     public static List<String> readFile(String file) {
